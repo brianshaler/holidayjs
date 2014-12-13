b0e891b97b3b47a5a0125e347c1d9a5da0db7ac6
Brian Shaler <brian@shaler.name>
2014-12-13 15:21:51 -0700

    missed some files

---
+++ client/game/game.js
@@ -0,0 +1,21 @@
+var Phaser = require('Phaser');
+var Load = require('./load');
+var Menu = require('./menu');
+var Play = require('./play');
+
+module.exports = function (elementId) {
+
+  var game = new Phaser.Game(400, 490, Phaser.AUTO, elementId);
+
+  // Our 'score' global variable
+  var score = 0;
+
+  // Define all the states
+  game.state.add('load', Load);
+  game.state.add('menu', Menu);
+  game.state.add('play', Play);
+
+  // Start with the 'load' state
+  game.state.start('load');
+
+};
